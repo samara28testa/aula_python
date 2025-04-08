5ac3b1e18874a22ce6a9a880465ba113e8f645ba
samara28testa <dataanalyst.samaratesta@gmail.com>
2025-04-08 09:05:13 +0200

    Add files via upload

--- 0704_cpt_05.py
+++ 0704_cpt_05.py
@@ -1,7 +1,17 @@
+#Compite con Liste
+
 numeri = [3, 1, 4, 2, 5]
 
 print(len(numeri))
 print(numeri.append(6))
+numeri.append((6))
+print(numeri)
+numeri.insert(2, 10)
+print(numeri)
+numeri.remove(4)
+print(numeri)
+numeri.sort()
+print(numeri)
 
 
 
