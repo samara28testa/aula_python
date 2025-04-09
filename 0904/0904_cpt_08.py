2db22a0c8e8cc4e0f73008767d944632af10b240
samara28testa <dataanalyst.samaratesta@gmail.com>
2025-04-09 15:49:19 +0200

    Add files via upload

--- 0904/0904_cpt_08.py
+++ 0904/0904_cpt_08.py
@@ -6,6 +6,12 @@ import numpy as np
 
 # Crea un array Numpy utilizzando arange e verifica il tipo di dato (dtype) e la forma (shape) dell'array.
 
+# Verifica il tipo di dato dell'array e stampa il risultado.
+
+# Cambia il tipo di dato dell'array in float64 e verifica di nuovo il tipo dato.
+
+# Stampa la forma dell'array.
+
 
 
 arr = np.arange(10, 50)
