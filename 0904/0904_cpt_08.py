1038aca217418a91d4117cba261a251cf4515013
samara28testa <dataanalyst.samaratesta@gmail.com>
2025-04-09 15:47:10 +0200

    Add files via upload

--- 0904/0904_cpt_08.py
+++ 0904/0904_cpt_08.py
@@ -4,10 +4,16 @@ import numpy as np
 
 # Es1: ndarray, dtype, shape, arrange
 
+# Crea un array Numpy utilizzando arange e verifica il tipo di dato (dtype) e la forma (shape) dell'array.
+
+
+
 arr = np.arange(10, 50)
 
 print("Tipo di dati:", arr.dtype)
+print("Forma dell'array:", arr.shape)
+
 
 arr_float = arr.astype(float)
 
-print("Nuovo tipo di dati:", arr_float)
+print("Nuovo tipo di dati:", arr_float.dtype)
