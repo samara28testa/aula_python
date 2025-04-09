53cc0757e92c43b95139205ca02cbd74df2d4d6a
samara28testa <dataanalyst.samaratesta@gmail.com>
2025-04-09 15:09:11 +0200

    Add files via upload

---
+++ 0904_cpt_06.py
@@ -0,0 +1,17 @@
+# Configurazione di NumPy
+
+import pip
+
+import numpy as np
+
+# Creazione di un array unidimensionale
+
+arr = np.array([1, 2, 3, 5])
+
+# Creazione di un array bidimensionale
+
+arra2d = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr)
+
+print(arra2d)
